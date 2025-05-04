9a34d935669e7f965322f6f795a3f63ad03beb2d
Darrell Jenkins <darrellajenkins@gmail.com>
2025-05-03 21:23:44 -0700

    Refacored unplaced code into a function called basic_score. Will later add code to account for the enhancers.

--- core.py
+++ core.py
@@ -51,14 +51,24 @@ def next_to(first, second):
 player_1 = player_1()
 p1_name = player_1[0]
 p1_charac = player_1[1]
-p1_score = 0
+
 
 player_2 = player_2()
 p2_name = player_2[0]
 p2_charac = player_2[1]
-p2_score = 0
+
 
 p = True
+
+def basic_score():
+    p1_score = 0
+    p2_score = 0
+    if p1_charac.power > p2_charac.power:
+        p1_score += p1_charac.power - p2_charac.power
+    elif p1_charac.power < p2_charac.power:
+        p2_score += p2_charac.power - p1_charac.power
+    return p1_score, p2_score
+
 while p:
     play = input("[P]lay, [S]top, or [Q]uit ")
     if play.lower() == 'q':
@@ -87,11 +97,7 @@ while p:
             print(p2_charac.strike())
         if next_to(p1_lands, p2_lands):
             print("Both players landed next to each other.")
-            if p1_charac.power > p2_charac.power:  # Make this a separate function later. One will always be larger than the other unless you write in option
-                # to use the enhancers.
-                p1_score += p1_charac.power - p2_charac.power
-            elif p1_charac.power < p2_charac.power:
-                p2_score += p2_charac.power - p1_charac.power
+            p1_score, p2_score = basic_score()
         elif p1_lands == p2_lands:
             print("Players landed in the same region. A special battle begins...")
             pass
